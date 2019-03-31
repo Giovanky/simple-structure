597f8b023fd9df784157260183c796e3d443478e
Giovanny Garzon <giovankyandres@gmail.com>
2019-03-31 20:20:43 -0300

    upgrade

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import { chaoMundo } from './other';
-
+import "../img/img.jpg";
 import "../scss/styles.scss"
 
 chaoMundo();
--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,52 @@ module.exports={
                   loader: 'babel-loader',
                   options: {
                     presets: ['@babel/preset-env']
-                  }
                 }
-              }
+            }
+            },
+            {
+                test: /\.(jpg|png|gif|jpeg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            useRelativePath: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(gif|png|jpeg|jpg|svg)$i/,
+                use: [
+                  'file-loader',
+                  {
+                    loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                            progressive: true,
+                            quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                            enabled: true,
+                            },
+                            pngquant: {
+                            quality: '65-90',
+                            speed: 4
+                            },
+                            gifsicle: {
+                            interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                            quality: 75
+                            }
+                        }
+                    },
+                ],
+            }
         ]
     },
     plugins: [
